c04fbbb0a63eb63fc1f0f8ee8296de30170eb3d8
Lucas <lucasgaarciaa@gmail.com>
2019-07-17 20:23:35 -0300

    lightbox

--- src/assets/snippets/newsletter/index.js
+++ src/assets/snippets/newsletter/index.js
@@ -21,3 +21,40 @@ $(".newsletter__form").submit(function(e){
     $(".conteudo-light-textos").addClass("hide");
     
 });
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') { c = c.substring(1); }
+        if (c.indexOf(name) == 0) { return c.substring(name.length, c.length); }
+    }
+
+    return "";
+} 
+
+if ($('.lightbox-out').length > 0) {
+
+    var lightboxtrue = Cookies.get('lightbox-out');
+    Cookies.set('lightbox-out', 'false');
+
+    $('.lightbox__close').click(function () {
+            $('.lightbox-out').addClass("hidden");
+            $('body').removeClass('news-on');
+            Cookies.set('lightbox-out', 'true', { expires: 30 });
+    });
+
+    if (!lightboxtrue) {
+        $('.lightbox-out').removeClass("hidden");
+    }
+}
